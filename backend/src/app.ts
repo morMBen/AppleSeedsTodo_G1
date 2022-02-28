bfbc03406be50e058cfa4802dcfe07b058b996ac
Liran-tal <taliran44@gmail.com>
2022-02-28 13:10:11 +0200

    add health check to user.routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,11 @@
-import express from 'express';
+import express, { Express } from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import { corsConfig } from './config';
+import Routes from './routes/routes';
 
-const app = express();
+const app: Express = express();
 
 app.use(cors(corsConfig));
 app.use(morgan('dev'));
@@ -12,7 +13,10 @@ app.use(helmet());
 app.use(express.json({}));
 app.use(express.urlencoded({ extended: false }));
 
+app.use('/api/users', Routes);
+
 app.use('*', ((err, req, res, next) => {
   res.status(500).send('Server Error');
 }) as express.ErrorRequestHandler);
+
 export { app };
---
+++ backend/src/routes/user.routes.ts
@@ -0,0 +1,8 @@
+import express, { Express, Request, Response } from "express";
+const Router = express.Router();
+
+Router.get('/healthcheck', (req: Request, res: Response): void => {
+  res.sendStatus(200);
+});
+
+export default Router;
