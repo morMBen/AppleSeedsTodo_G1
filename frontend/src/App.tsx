9a66635e0f1f3de09589ab8ffed74dd2721d3195
Yevgeny-Sh <yevgst7@gmail.com>
2022-03-01 15:24:32 +0200

    welcome page version1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
-
+import Welcomepage from './Welcomepage';
 import { useDispatch, useSelector } from 'react-redux';
 import { addTask } from './redux/actions';
 import { tasksReducer } from './redux/taskReducer';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Welcomepage />
+    </div>
+  );
 }
 
 export default App;
