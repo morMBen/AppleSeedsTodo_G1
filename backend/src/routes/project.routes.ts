a694611d53eeedbed69d4d577bf63ecc4f59c6d2
Liran-tal <taliran44@gmail.com>
2022-03-01 13:28:11 +0200

    TG1-71: add create project routing

--- backend/src/routes/project.routes.ts
+++ backend/src/routes/project.routes.ts
@@ -1,9 +1,10 @@
 import express, { Request, Response } from "express";
 import validateResource from "../middlewere/validateResource";
 import { 
-  
+  createNewProjectHandler,  
   getAllProjectsDetailsHandler,
 } from "../controllers/projects.controller";
+import { createProjectSchema } from "../schema/project.shema";
 
 
 const Router = express.Router();
@@ -12,8 +13,11 @@ Router.get('/healthcheck', (req: Request, res: Response): void => {
   res.sendStatus(200);
 });
 
+Router.post('/create-new-project', 
+validateResource(createProjectSchema),
+  createNewProjectHandler,
+);
+
 Router.get('/get-all-projects-meta/:projectId', getAllProjectsDetailsHandler);
-
-
 
 export default Router;
--- backend/src/services/task.service.ts
+++ backend/src/services/task.service.ts
@@ -1,6 +1,4 @@
-import mockProjects from '../mock-data';
 
 export async function getTasksByProjectService(project: object) {
-  const tasks = mockProjects.find((project) => project._id === project._id);
-  return tasks
+
 }
